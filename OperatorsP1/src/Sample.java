b6b11fac92609dff499f81dc02800a7a76b1cc2e
OnlineTrainer <mohamedtechdata@gmail.com>
2022-12-13 07:56:45 +0530

    13-12-2022

--- OperatorsP1/src/Sample.java
+++ OperatorsP1/src/Sample.java
@@ -23,6 +23,15 @@ class Demo
 		System.out.println("logicalOR::"+((a2>b2)||(b2>a2)));
 		System.out.println("LogicalNot::"+(!(a2>b2)));
 	}
+	void bitwise()
+	{
+		int a=2,b=3;
+		System.out.println("BitWiseAND::"+(a&b));
+		System.out.println("BitWiseOR::"+(a|b));
+		System.out.println("BitWiseXOR::"+(a^b));
+		System.out.println("BitWiseleftshift::"+(a<<1));
+		System.out.println("BitWiserightshift::"+(a>>1));
+	}
 }
 public class Sample {
 
@@ -31,7 +40,8 @@ public class Sample {
 		Demo f1=new Demo();
 		//f1.arithmeticOperators();
 		//f1.relationalOperators();
-		f1.logicalOperators();
+		//f1.logicalOperators();
+		f1.bitwise();
 
 	}
 
