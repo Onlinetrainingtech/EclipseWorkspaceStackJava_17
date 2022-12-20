39284591ce68757abf489121dc3082394d44312a
OnlineTrainer <mohamedtechdata@gmail.com>
2022-12-20 07:45:38 +0530

    20-12-22

---
+++ LoopingP3/src/Sample.java
@@ -0,0 +1,20 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=5;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("I value is::Even::"+i);
+			}
+			else
+			{
+				System.out.println("I value is::Odd::"+i);
+			}
+		}
+
+	}
+
+}
---
+++ LoopingWhileP1/src/Sample.java
@@ -0,0 +1,21 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		int n=5;
+//		while(i<=n)
+//		{
+//			System.out.println("I value is::"+i);
+//			i++;
+//		}
+		do
+		{
+			System.out.println("I value is:::"+i);
+			i++;
+		}while(i<=n);
+
+	}
+
+}
